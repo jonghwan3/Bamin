53854c39f8a495cd82b04ed848270c4e21640a68
jonghwan3 <pjhan93@hanyang.ac.kr>
2022-07-08 09:32:08 +0900

    Initial Commit

---
+++ Bamin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bamin
+//
+//  Created by 박종환 on 2022/07/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(1)
+        print(2)
+        print(3)
+        
+        print(4)
+        print(5)
+    }
+
+
+}
+
